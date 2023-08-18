b84e576c7ed04af05aaf4e0fabd7c91315424712
Dima Ochkivskyi <dima.ochkivskyi.lun.au>
2023-08-18 19:16:38 +0300

    add color highlight plugin for nvim

---
+++ nvim/.config/nvim/lua/plugins/custom.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "brenoprata10/nvim-highlight-colors",
+    opts = {
+      render = "background",
+    },
+    keys = {
+      { "<leader>cc", "<cmd>HighlightColorsToggle<cr>", desc = "Toggle color highlight" },
+    },
+  },
+}
