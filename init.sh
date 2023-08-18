cdc0cb1df45876eff69c0749933019676cd2f362
Dima Ochkivskyi <dima.ochkivskyi.lun.au>
2023-08-18 18:10:36 +0300

    add basic init script

---
+++ init.sh
@@ -0,0 +1,30 @@
+log_file=~/install_progress_log.txt
+
+sudo apt -y install zsh
+if type -p zsh > /dev/null; then
+    echo "zsh Installed" >> $log_file
+else
+    echo "zsh FAILED TO INSTALL!!!" >> $log_file
+fi
+
+sudo apt -y install stow
+if type -p stow > /dev/null; then
+    echo "stow Installed" >> $log_file
+else
+    echo "stow FAILED TO INSTALL!!!" >> $log_file
+fi
+
+# ohmyzsh:
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# stow dotfiles
+stow zsh
+stow nvim
+stow rio
+stow prettier
+
+# install nvm
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh)"
+nvm install stable
+nvm use stable
+
