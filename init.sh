0ab052197244e8ce66770898ab596403ee843f09
Dima Ochkivskyi <dima.ochkivskyi.lun.au>
2023-08-18 19:16:11 +0300

    install JetBrains Mono

--- init.sh
+++ init.sh
@@ -1,18 +1,21 @@
 log_file=~/install_progress_log.txt
 
 sudo apt -y install zsh
-if type -p zsh > /dev/null; then
-    echo "zsh Installed" >> $log_file
+if type -p zsh >/dev/null; then
+	echo "zsh Installed" >>$log_file
 else
-    echo "zsh FAILED TO INSTALL!!!" >> $log_file
+	echo "zsh FAILED TO INSTALL!!!" >>$log_file
 fi
 
 sudo apt -y install stow
-if type -p stow > /dev/null; then
-    echo "stow Installed" >> $log_file
+if type -p stow >/dev/null; then
+	echo "stow Installed" >>$log_file
 else
-    echo "stow FAILED TO INSTALL!!!" >> $log_file
+	echo "stow FAILED TO INSTALL!!!" >>$log_file
 fi
+
+# JetBrains Mono font
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
 
 # ohmyzsh:
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
@@ -27,4 +30,3 @@ stow prettier
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh)"
 nvm install stable
 nvm use stable
-
