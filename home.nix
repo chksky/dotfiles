95982518368f586aa24c721c1677d7368fa3eb97
Dima Ochkivskyi <github@chksky.com>
2025-01-25 17:50:39 +0200

    add ghostty config

--- home.nix
+++ home.nix
@@ -108,14 +108,14 @@
     difftastic.enable = true;
   };
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
+
     ".config/zed".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.dotfiles/zed";
+    ".config/ghostty".source = config.lib.file.mkOutOfStoreSymlink  "${config.home.homeDirectory}/.dotfiles/ghostty";
 
     # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
